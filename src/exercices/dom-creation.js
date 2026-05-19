76e16b13225ef2f0d699b21d37c0a332b39ea396
jefff303 <ksora@outlook.fr>
2026-05-19 16:24:19 +0200

    Update : exemple Dom creation et Event

--- src/exercices/dom-creation.js
+++ src/exercices/dom-creation.js
@@ -1,16 +1,25 @@
+//!-----------------------Exercice DOM afficher profil utilisateur-----------------------
+
 const userData = {
-    name: 'John delavega',
+    name: 'Janine delavega',
     email: 'john.doe@example.com',
     age: 25,
     dob: '08/02/1989',
-    active: true,
+    active: false,
     img: 'https://www.boredpanda.com/blog/wp-content/uploads/2022/06/funny-low-cost-cosplay-pics-62a744d39c80a__700.jpg'
   };
   // JS qui va customiser la div du profile utilisateur
   let divUser = document.querySelector('.userProfile');
+  divUser.setAttribute('productId','9876543E4R567')
   console.log(divUser);
 
-  divUser.style.backgroundColor = `background-color: #4158D0`;
+
+  divUser.addEventListener('click',()=>{
+    console.log('CA CLICK');
+    divUser.style.backgroundColor = 'red';
+  })
+
+  divUser.style.backgroundColor = `#4158D0`;
   divUser.style.backgroundImage = `linear-gradient(43deg, #4158D0 0%, #C850C0 46%, #FFCC70 100%)`;
   divUser.style.color = `white`;
   divUser.style.width = '500px';
@@ -22,11 +31,12 @@ const userData = {
   imgTemplate.alt = userData.name;
   imgTemplate.style.height = '500px';
   imgTemplate.style.width = '500px';
-  divUser.append(imgTemplate);
+  divUser.appendChild(imgTemplate);
   // JS crée le titre du name
   const nameTemplate = document.createElement('h1');//phase 1 creation
   nameTemplate.innerText = userData['name'];
-  divUser.append(nameTemplate);
+//   nameTemplate.innerText = userData.name;
+  divUser.appendChild(nameTemplate);
   // JS crée le titre du email
   const emailTemplate = document.createElement('h2');//phase 1 creation
   emailTemplate.innerText = userData.email;
@@ -34,13 +44,46 @@ const userData = {
   // JS crée le titre du age
   const ageTemplate = document.createElement('h2');//phase 1 creation
   ageTemplate.innerText = userData.age;
-  divUser.append(ageTemplate);
+  divUser.appendChild(ageTemplate);
   // JS crée le titre du dob
   const dobTemplate = document.createElement('h2');//phase 1 creation
   dobTemplate.innerText = userData.dob;
-  divUser.append(dobTemplate);
+  divUser.appendChild(dobTemplate);
   // JS crée le titre du active
   const activeTemplate = document.createElement('h2');//phase 1 creation
   // activeTemplate.innerText = userData.active;
   activeTemplate.innerText = userData.active ? 'OnLine' : 'OffLine';
+//   activeTemplate.innerText = userData.active===true ? 'OnLine' : 'OffLine';
   divUser.append(activeTemplate);
+
+
+//!-----------------------Exercice Fonction ajouterTexte-----------------------
+
+
+function ajouterTexte(prenom, nom) {
+  const containerAdd = document.querySelector('#textAdd');
+  console.log(containerAdd);
+  // On créer un élément p
+  const nouveauParagraphe = document.createElement('p');
+// On créer un élément strong
+const strong = document.createElement('strong');
+// On ajoute le texte au strong
+strong.textContent = prenom;
+// On ajoute le strong au p
+nouveauParagraphe.append(strong);
+// On ajoute le texte au p
+nouveauParagraphe.append(` - ${nom}`);
+// On ajoute le p au body
+containerAdd.append(nouveauParagraphe);
+}
+
+
+ajouterTexte('Daniel','Gracia');
+ajouterTexte('Jarry','La Borne');
+ajouterTexte('Jean-Claude','Vandamme');
+ajouterTexte('Bernard','De LaVitreArrière');
+
+// ajouterTexte('Denis','La Mailce');
+// ajouterTexte('Janine','Delavega');
+
+
---
+++ src/exercices/dom-events.js
@@ -0,0 +1,3 @@
+// TODO : On récupère le Titre
+// TODO : On place un écouteurs d'évènement sur le Titre (qui réagit au click)
+// TODO : Dans la fonction de l'écouteur d'évènement on se débrouille pour modifier le texte du titre
