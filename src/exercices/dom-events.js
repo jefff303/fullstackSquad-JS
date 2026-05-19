a36510bcd6b6a78166f700786e8bb877798935b8
jefff303 <ksora@outlook.fr>
2026-05-19 18:44:42 +0200

    Update : Lessons , Exercices
    pages de leçon sur les modification d'attributs et sur les events
    Pages d'exercices sur les Events

--- src/exercices/dom-events.js
+++ src/exercices/dom-events.js
@@ -1,3 +1,20 @@
 // TODO : On récupère le Titre
 // TODO : On place un écouteurs d'évènement sur le Titre (qui réagit au click)
 // TODO : Dans la fonction de l'écouteur d'évènement on se débrouille pour modifier le texte du titre
+//TODO : Bonus comment faire pour quand on reclick ca remet le contenu de base du titre ;) ?    
+
+// On selectionne le titre
+const mainTitle = document.querySelector('h1');
+let isClicked = false;
+// console.log(mainTitle);
+// On place l'écouteur d'events
+
+
+mainTitle.addEventListener('click',()=>{
+    console.log('Ok le titre est clickable');
+    // ON modifie le innerText de ce meme titre
+    // mainTitle.innerText = '😄';
+    mainTitle.innerText = isClicked  ? '😄' : 'Hello World'
+    isClicked = !isClicked;
+});
+
